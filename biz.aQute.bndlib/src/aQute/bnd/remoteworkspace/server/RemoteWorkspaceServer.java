c1b27afe2e48a007cc89501668e1c39392a86fef
John Oliver <johno@insightfullogic.com>
2018-11-29 14:06:28 +0000

    add more debug logging to files

--- biz.aQute.bndlib/src/aQute/bnd/remoteworkspace/server/RemoteWorkspaceServer.java
+++ biz.aQute.bndlib/src/aQute/bnd/remoteworkspace/server/RemoteWorkspaceServer.java
@@ -88,10 +88,8 @@ public class RemoteWorkspaceServer implements Closeable {
 				"Cannot create the remote workspace directory with port numbers " + remotews);
 
 		System.out.println("Creating port file at: " + remotews.getAbsolutePath() + " " + server.getLocalPort())
-		remotewsPort = new File(remotews, server.getLocalPort() + "");
 		IO.store(server.getLocalPort() + "", remotewsPort);
 		logger.info("Opening remote workspace server {}", remotewsPort);
-		System.out.println("Written to: " + remotewsPort.getAbsolutePath())
 		remotewsPort.deleteOnExit();
 	}
 
